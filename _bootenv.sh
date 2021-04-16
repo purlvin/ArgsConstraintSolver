7fd20a912fa412ebfc9de2df0fc1d02f8a2deb88
Purlvin Zhang <Purlvin.Zhang@amd.com>
2021-04-16 18:17:06 -0400

    Added bootenv

---
+++ _bootenv.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Common
+alias h="history"
+alias g="gvim -p"
+alias findex="find . -type f | grep \!:*"
+# Run_test
+alias run_test='$ANCHOR_smu_infra/passphrase/run_test.py'
+
+# GO alias
+alias go_bia='cd ${ROOT}/common/tmp/${ANCHOR_connectivity}/connectivity/src/bia/flows/logical_view'
+alias go_sim='cd ${ROOT}/config/smu*_uvm/pub/sim'
+alias go_run='cd ${ROOT}/config/smu*_uvm/run/bootcode'
+
